9b68a5ff8280a40028e657e05ba96aa408ce635f
amarachiugwu <marycynthiamara2@gmail.com>
2024-06-28 16:48:43 +0100

    guest user

---
+++ src/components/dashboard/Assets.tsx
@@ -0,0 +1,96 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Assets() {
+  return (
+    <div className="w-2/5 flex flex-col gap-4">
+        <p className='font-semibold'>Supported assets</p>
+
+        <div className="w-full flex flex-col rounded-lg gap-4 bg-tertiary-6 p-6">
+            
+            <div className="w-full flex justify-between items-center">
+            <div className="flex gap-4 items-center">
+                <Image 
+                width={48}
+                height={48}
+                src="/images/bitcoin.png"
+                alt="bitcoin"
+                />
+                <div className="flex flex-col gap-1 ">
+                <p>USDT </p>
+                </div>
+            </div>
+
+            <p>23.00%</p>
+            </div>
+
+            <div className="w-full flex justify-between  items-center">
+            <div className="flex gap-4 items-center">
+                <Image
+                width={48}
+                height={48}
+                src="/images/bitcoin.png"
+                alt="bitcoin"
+                />
+                <div className="flex flex-col gap-1 ">
+                <p>USDT </p>
+                </div>
+            </div>
+
+            <p>23.00%</p>
+            </div>
+
+
+            <div className="w-full flex justify-between items-center">
+            <div className="flex gap-4 items-center">
+                <Image 
+                width={48}
+                height={48}
+                src="/images/bitcoin.png"
+                alt="bitcoin"
+                />
+                <div className="flex flex-col gap-1 ">
+                <p>USDT </p>
+                </div>
+            </div>
+
+            <p>23.00%</p>
+            </div>
+
+
+            <div className="w-full flex justify-between items-center">
+            <div className="flex gap-4 items-center">
+                <Image 
+                width={48}
+                height={48}
+                src="/images/bitcoin.png"
+                alt="bitcoin"
+                />
+                <div className="flex flex-col gap-1 ">
+                <p>USDT </p>
+                </div>
+            </div>
+
+            <p>23.00%</p>
+            </div>
+
+            <div className="w-full flex justify-between items-center">
+            <div className="flex gap-4 items-center">
+                <Image 
+                width={48}
+                height={48}
+                src="/images/bitcoin.png"
+                alt="bitcoin"
+                />
+                <div className="flex flex-col gap-1 ">
+                <p>USDT </p>
+                </div>
+            </div>
+
+            <p>23.00%</p>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
---
+++ src/components/dashboard/GuestLayout.tsx
@@ -0,0 +1,30 @@
+import React, { type ReactNode } from 'react'
+import Assets from './Assets'
+import GuestBalance from './GuestBalance'
+
+export default function GuestLayout(props: { children: ReactNode }) {
+  return (
+    <div className='w-4/5 flex flex-col'>
+              {/* Balances */}
+              <GuestBalance />
+
+              {/* history and token */}
+              {<section className="w-full m-h-screen w-4/4 px-6 py-0">
+                    <div className='flex gap-4 w-full'>
+                    
+                        <div className="w-3/5 flex flex-col gap-4">
+                            
+                            <p className='font-semibold'>Information</p>
+
+                            <div className="w-full flex flex-col rounded-lg bg-tertiary-6">
+                                {props.children}
+                            </div>
+                        </div>
+
+                        <Assets />
+                    </div>
+              </section>}
+
+            </div>
+  )
+}
