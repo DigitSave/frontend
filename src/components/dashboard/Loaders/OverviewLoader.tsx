f6e068124c4984641905ccfeaf2ba78f9b6496fc
amarachiugwu <marycynthiamara2@gmail.com>
2024-06-28 16:49:30 +0100

    overview loaders

--- src/components/dashboard/Loaders/OverviewLoader.tsx
+++ src/components/dashboard/Loaders/OverviewLoader.tsx
@@ -28,10 +28,6 @@ export default function OverviewLoader() {
                     <div className="animate-pulse flex space-x-4">
                         <div className="flex-1 space-y-6 py-1">
                             <div className="space-y-6">
-                                {/* <div className="grid grid-cols-3 gap-4">
-                                    <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                                    <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                                </div> */}
                                 <div className="h-2 w-3/5 bg-slate-700 rounded"></div>
                                 <div className="h-8 w-2/5 bg-slate-700 rounded"></div>
                             </div>
