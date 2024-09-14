d1fffc0dba4c72cd793e4f8d940083e40ad00665
amarachiugwu <marycynthiamara2@gmail.com>
2024-09-14 06:28:35 +0100

    merge fix

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-"use client";
 
 import Header from "@/components/dashboard/Header";
 import Sidebar from "@/components/dashboard/Sidebar";
@@ -36,25 +35,15 @@ import Web3 from "web3";
 import SavingListLoader from "@/components/dashboard/Loaders/SavingListLoader";
 import { NumericFormat } from "react-number-format";
 import { useRouter } from "next/navigation";
-import Header from "@/components/dashboard/Header";
-import Sidebar from "@/components/dashboard/Sidebar";
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { useAccount } from "wagmi";
-import { Circle, WalletIconPlain } from "@/icon";
-import { AnimatePresence, motion } from "framer-motion";
-import MobileSidebar from "@/components/dashboard/MobileSidebar";
 
 export default function Activities() {
   const router = useRouter();
 
   const { isDisconnected } = useAccount();
-  const [navOpen, setNavOpen] = useState(false);
 
   useEffect(() => {
     if (isDisconnected) {
       router.push("/dashboard");
-      router.push("/");
     }
   }, [isDisconnected, router]);
   const { chainUrl } = useChainUrl();
@@ -509,137 +498,6 @@ export default function Activities() {
                             )}
                         </tbody>
                       </table>
-    <main className="text-neutral-2">
-      <Header navOpen={navOpen} setNavOpen={setNavOpen} />
-      <section className="flex border-t border-tertiary-6">
-        <div className="w-1/5 hidden lg:block">
-          <div className="w-1/5 fixed">
-            <Sidebar />
-          </div>
-        </div>
-        <AnimatePresence>
-          {navOpen && (
-            <div className="w-full h-screen lg:hidden fixed block  z-20">
-              <div
-                onClick={() => setNavOpen(!navOpen)}
-                className="h-screen w-full cursor-pointer bg-transparent backdrop-blur-sm fixed z-20"
-              ></div>
-              <motion.div
-                exit={{ width: 0, opacity: 0, transition: { duration: 0.6 } }}
-                animate={{
-                  opacity: 1,
-                  transition: { duration: 0.9 },
-                }}
-                initial={{ opacity: 0 }}
-                className=" sm:w-1/3 w-full fixed bg-tertiary-0 z-40"
-              >
-                {" "}
-                <MobileSidebar />
-              </motion.div>
-            </div>
-          )}
-        </AnimatePresence>
-
-        <div className="w-4/5 flex flex-col">
-          {/* history */}
-          <section className="w-full m-h-screen w-4/4 px-6 py-10">
-            <div className="flex gap-4 w-full">
-              <div className="w-full flex flex-col gap-4">
-                <p className="font-semibold">Recent activities</p>
-                <p className="text-tertiary-4">
-                  Let’s see how well you are doing.
-                </p>
-
-                <div className="mt-4 w-full flex flex-col rounded-lg bg-tertiary-6">
-                  <div className="text-sm p-6">
-                    <div className="flex flex-col gap-6">
-                      <div className="flex gap-8 justify-between items-center">
-                        <div className="flex gap-4">
-                          <WalletIconPlain />
-                          <div className="flex flex-col gap-1 ">
-                            <p>Savings account credited</p>
-                            <p className="text-xs">2 days ago</p>
-                          </div>
-                        </div>
-
-                        <div className="flex gap-2 py-1 px-3 items-center bg-tertiary-7 rounded-xl">
-                          <Circle />
-                          <p>Successful</p>
-                        </div>
-
-                        <p>$120</p>
-
-                        <p>24-04-2024</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="text-sm p-6">
-                    <div className="flex flex-col gap-6">
-                      <div className="flex gap-8 justify-between items-center">
-                        <div className="flex gap-4">
-                          <WalletIconPlain />
-                          <div className="flex flex-col gap-1 ">
-                            <p>Savings account credited</p>
-                            <p className="text-xs">2 days ago</p>
-                          </div>
-                        </div>
-
-                        <div className="flex gap-2 py-1 px-3 items-center bg-tertiary-7 rounded-xl">
-                          <Circle />
-                          <p>Successful</p>
-                        </div>
-
-                        <p>$120</p>
-
-                        <p>24-04-2024</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="text-sm p-6">
-                    <div className="flex flex-col gap-6">
-                      <div className="flex gap-8 justify-between items-center">
-                        <div className="flex gap-4">
-                          <WalletIconPlain />
-                          <div className="flex flex-col gap-1 ">
-                            <p>Savings account credited</p>
-                            <p className="text-xs">2 days ago</p>
-                          </div>
-                        </div>
-
-                        <div className="flex gap-2 py-1 px-3 items-center bg-tertiary-7 rounded-xl">
-                          <Circle />
-                          <p>Successful</p>
-                        </div>
-
-                        <p>$120</p>
-
-                        <p>24-04-2024</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="text-sm p-6">
-                    <div className="flex flex-col gap-6">
-                      <div className="flex gap-8 justify-between items-center">
-                        <div className="flex gap-4">
-                          <WalletIconPlain />
-                          <div className="flex flex-col gap-1 ">
-                            <p>Savings account credited</p>
-                            <p className="text-xs">2 days ago</p>
-                          </div>
-                        </div>
-
-                        <div className="flex gap-2 py-1 px-3 items-center bg-tertiary-7 rounded-xl">
-                          <Circle />
-                          <p>Successful</p>
-                        </div>
-
-                        <p>$120</p>
-
-                        <p>24-04-2024</p>
-                      </div>
                     </div>
                   </div>
                 </div>
@@ -652,8 +510,6 @@ export default function Activities() {
           <div className="flex w-4/5 flex-col item-center justify-center text-center gap-6">
             <div className="flex justify-center w-full">
               <FileIcon />
-                </div>
-              </div>
             </div>
 
             <p className="text-neutral-3 text-xl font-medium">
@@ -680,10 +536,6 @@ export default function Activities() {
         {activitiesError}
         {/* {activitiesData } */}
       </section>
-          </section>
-        </div>
-      </section>
     </main>
-  );
   );
 }
--- src/app/create-save/page.tsx
+++ src/app/create-save/page.tsx
@@ -5,42 +5,20 @@ import { BackArrowIcon } from "@/icon";
 import Link from "next/link";
 import CreateSaveForm from "@/components/dashboard/CreateSaveForm";
 import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import MobileSidebar from "@/components/dashboard/MobileSidebar";
 
 export default function CreateSave() {
   const [navOpen, setNavOpen] = useState(false);
+
   return (
     <main className="text-neutral-2">
       <Header navOpen={navOpen} setNavOpen={setNavOpen} />
       <section className="flex min-h-screen border-t border-tertiary-6">
-        <div className="w-1/5 hidden lg:block">
+        <div className="w-1/5">
           <div className="w-1/5 fixed">
             <Sidebar />
           </div>
         </div>
-        <AnimatePresence>
-          {navOpen && (
-            <div className="w-full h-screen lg:hidden fixed block  z-20">
-              <div
-                onClick={() => setNavOpen(!navOpen)}
-                className="h-screen w-full cursor-pointer bg-transparent backdrop-blur-sm fixed z-20"
-              ></div>
-              <motion.div
-                exit={{ width: 0, opacity: 0, transition: { duration: 0.6 } }}
-                animate={{
-                  opacity: 1,
-                  transition: { duration: 0.9 },
-                }}
-                initial={{ opacity: 0 }}
-                className=" sm:w-1/3 w-full fixed bg-tertiary-0 z-40"
-              >
-                {" "}
-                <MobileSidebar />
-              </motion.div>
-            </div>
-          )}
-        </AnimatePresence>
+
         <div className="w-4/5 flex flex-col">
           <Link href="/save" className="flex gap-2 p-6 pb-2 items-center">
             <BackArrowIcon />
